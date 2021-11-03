eb5cf4be289efa353fefd3ed2da69a2a4467c192
Cho Chan Hee <cksgml4424@gmail.com>
2021-11-03 11:30:48 +0900

    모델 service 주석 내용 추가

--- src/model/service/model.service.ts
+++ src/model/service/model.service.ts
@@ -11,10 +11,22 @@ export class ModelService {
         private modelRepository: Repository<Model>) {}
 
 
+    /**    
+     * Method : getAll
+     * 2021.11.03
+     * description: 모델 리스트 조회 By Cho
+     * @returns 
+     */
     getAll() {
         return this.modelRepository.find();
-    }    
-
+    }  
+    
+    
+    /**
+     * 모델 상세 조회
+     * @param idx 
+     * @returns 
+     */
     getOne(idx: number) {
         const model = this.modelRepository.createQueryBuilder()
         .where('idx = :idx', {idx: idx})
